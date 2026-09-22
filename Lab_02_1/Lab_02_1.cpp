77b86bdccf4e262274448a8b05fe361a6b6431f3
Roman Salamakha <sadlime012@gmail.com>
2026-09-22 19:03:35 +0300

    Lab_02_1: initial project

---
+++ Lab_02_1/Lab_02_1.cpp
@@ -0,0 +1,29 @@
+// Lab_02_1.cpp
+// Саламаха Роман
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 27
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double alpha; // вхідний параметр (кут у радіанах)
+    double z1;    // результат обчислення 1-го виразу
+    double z2;    // результат обчислення 2-го виразу
+
+    cout << "alpha = "; cin >> alpha;
+
+    z1 = cos(3 * M_PI / 8 - alpha / 4) * cos(3 * M_PI / 8 - alpha / 4)
+       - cos(11 * M_PI / 8 + alpha / 4) * cos(11 * M_PI / 8 + alpha / 4);
+    z2 = sqrt(2.) / 2 * sin(alpha / 2);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
